6f9688b8a3232243672455c27e6cfc1d7d8d1afe
一丝 <percyley@qq.com>
2022-08-11 12:30:53 +0800

    fix: use the correct resvg-js parameters to avoid PNG jaggies

--- scripts/benchmark/resvg.js
+++ scripts/benchmark/resvg.js
@@ -6,13 +6,9 @@ export default svg => (
 			loadSystemFonts: false,
 		},
 		logLevel: 'off',
-		imageRendering: 1,
-		textRendering: 0,
-		shapeRendering: 0,
 		fitTo: {
 			mode: 'width',
 			value: 800,
 		},
-		dpi: 2400,
 	}).render().asPng()
 );
